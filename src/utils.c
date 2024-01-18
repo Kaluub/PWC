0377e2056e93c8ecf0449d5bca88d1628d8662b1
Kaluub <kaluubs@gmail.com>
2024-01-18 08:46:44 -0500

    Use macro for min/max, update workflows.

--- src/utils.c
+++ src/utils.c
@@ -2,14 +2,6 @@
 #include <math.h>
 #include <malloc.h>
 
-int min(int a, int b) {
-    return a < b ? a : b;
-}
-
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
 Vector2 GetPointInMap(Map* map) {
     return (Vector2) {GetRandomValue(map->area.x, map->area.x + map->area.width), GetRandomValue(map->area.y, map->area.y + map->area.height)};
 }
--- src/utils.h
+++ src/utils.h
@@ -3,12 +3,13 @@
 #include "raylib.h"
 #include "map/map.h"
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 typedef struct CirclePoint {
     Vector2 position;
     float angle;
 } CirclePoint;
 
-int min(int a, int b);
-int max(int a, int b);
 Vector2 GetPointInMap(Map* map);
 CirclePoint* GetPointsOnCircle(int count, float arcLength, float centerAngle, Vector2 position, float radius);
